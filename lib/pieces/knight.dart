0bb53789f1bc8cd3a2db6a640d2d7904fcd307a0
Rafael Mazolini Fernandes <102422449+RafaMazoliniF@users.noreply.github.com>
2023-11-15 09:30:02 -0300

    implement knight movement

--- lib/pieces/knight.dart
+++ lib/pieces/knight.dart
@@ -1,7 +1,7 @@
-import 'package:chess/Models/chessboardmodel.dart';
 import 'package:chess/pieces/piece.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:collection/collection.dart';
 
 class Knight extends StatelessWidget with Piece {
   final String color;
@@ -16,8 +16,41 @@ class Knight extends StatelessWidget with Piece {
   }
 
   @override
-  bool canMove(
-      int fromX, int fromY, int toX, int toY, List<List<String>> layout) {
-    return false;
+  bool canMove(int i1, int j1, int i2, int j2, List<List<String>> layout) {
+    List<List<int>> movePossibilities = [
+      [i1 + 2, j1 + 1],
+      [i1 + 2, j1 - 1],
+      [i1 - 2, j1 + 1],
+      [i1 - 2, j1 - 1],
+      [i1 + 1, j1 + 2],
+      [i1 - 1, j1 + 2],
+      [i1 + 1, j1 - 2],
+      [i1 - 1, j1 - 2]
+    ];
+
+    // New legal possibilities
+    movePossibilities = movePossibilities
+        .where((possibility) =>
+            isWithinBounds(possibility) && !isFriend(possibility, layout))
+        .toList();
+
+    //Verification
+    if (!movePossibilities
+        .any((element) => const ListEquality().equals(element, [i2, j2]))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool isWithinBounds(List<int> position) {
+    return position[0] >= 0 &&
+        position[0] < 8 &&
+        position[1] >= 0 &&
+        position[1] < 8;
+  }
+
+  bool isFriend(List<int> position, List<List<String>> layout) {
+    return layout[position[0]][position[1]][0] == color[0];
   }
 }
